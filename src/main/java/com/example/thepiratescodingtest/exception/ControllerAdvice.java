e642ec72e843835115b4ad3fc7789f0acf177b1d
davidko <ksb3602@gmail.com>
2022-03-25 02:31:18 +0900

    코드 클리닝

--- src/main/java/com/example/thepiratescodingtest/exception/ControllerAdvice.java
+++ src/main/java/com/example/thepiratescodingtest/exception/ControllerAdvice.java
@@ -2,15 +2,10 @@ package com.example.thepiratescodingtest.exception;
 
 import com.example.thepiratescodingtest.controller.MainController;
 import lombok.extern.slf4j.Slf4j;
-import org.omg.CORBA.UserException;
 import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
-
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
 
 @Slf4j
 @RestControllerAdvice(assignableTypes = MainController.class) // 지정하지 않으면 글로벌
@@ -18,15 +13,9 @@ public class ControllerAdvice {
 
     @ResponseStatus(HttpStatus.BAD_REQUEST)
     @ExceptionHandler(IllegalArgumentException.class)
-    public ErrorResult illegalExResolver(HttpServletRequest request,
-                                         HttpServletResponse response,
-                                         IllegalArgumentException e) {
+    public ErrorResult illegalExResolver(IllegalArgumentException e) {
         log.info("illegalExResolver Start!");
         return new ErrorResult(HttpStatus.BAD_REQUEST,
                 e.getMessage());
-    }
-
-    @ResponseStatus(value = HttpStatus.BAD_REQUEST, reason = "error.bad")
-    public class BadRequestException extends RuntimeException{
     }
 }
